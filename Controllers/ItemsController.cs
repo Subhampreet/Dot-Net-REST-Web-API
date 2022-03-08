15d178dda7f3ce31e67b5a38e4cf09aef72e6829
Subhampreet <subhampreet18@gmail.com>
2022-03-08 21:21:21 +0530

    Update Method Added

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -54,5 +54,23 @@ namespace Catalog.Controllers
 
             return CreatedAtAction(nameof(GetItem), new {id = item.Id}, item.AsDto());
         }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto){
+            var existingItem = repository.GetItem(id);
+            if(existingItem is null)
+            {
+                return NotFound();
+            }
+
+            Item UpdatedItem = existingItem with {
+                Name = itemDto.Name,
+                Price = itemDto.Price
+            };
+
+            repository.UpdateItem(UpdatedItem);
+
+            return NoContent();
+        }
     }
 }
