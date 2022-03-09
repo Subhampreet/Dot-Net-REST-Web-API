192969c5390b29bbe3da32f41fac2ca9bdadfd83
Subhampreet <subhampreet18@gmail.com>
2022-03-10 00:20:21 +0530

    Delete Operation

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -72,5 +72,18 @@ namespace Catalog.Controllers
 
             return NoContent();
         }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(Guid id)
+        {
+            var existingItem = repository.GetItem(id);
+            if(existingItem is null)
+            {
+                return NotFound();
+            }
+
+            repository.DeleteItem(id);
+            return NoContent();
+        }
     }
 }
